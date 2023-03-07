0a8535738ef9e50fc4f521b1f953e17b8bc5e636
Adeyemi Oluwagbenga Daniel <adeyemidanielgad@gmail.com>
2023-03-07 16:14:21 +0100

    update

--- calculator.c
+++ calculator.c
@@ -1,18 +1,45 @@
 #include <stdio.h>
 
-int main(void) {
-    int n1, n2;
-    char n3;
+int main(void) 
+{
+	double num1, num2, ans;
+	char operator;
 
-    printf("Enter the first number: ");
-    scanf("%d", &n1);
+	printf("Enter the first number: ");
+	scanf("%lf", &num1);
 
-    printf("Enter the second number: ");
-    scanf("%d", &n2);
+	printf("Enter the second number: ");
+	scanf("%lf", &num2);
 
-    printf("Enter:\n(+) for Sum;\n(-) for Difference;\n(*) for Product;(/) for Quotient;\n ");
-    scanf("%c", &n3)
+	printf("What will you like to do;\nEnter:\n+ for Sum\n- for Difference\n* for Product\n/ for Quotient\n: ");
+	scanf("%s", &operator);
 
-    printf("%c", n3);
+	switch (operator)
+	{
+	case '+':
+		ans = num1 + num2;
+		printf("%lf + %lf = %.3lf", num1, num2, ans);
+		break;
+
+	case '-':
+		ans = num1 - num2;
+		printf("%lf - %lf = %.3lf", num1, num2, ans);
+		break;
+
+	case '*':
+		ans = num1 * num2;
+		printf("%lf * %lf = %.3lf", num1, num2, ans);
+		break;
+	case '/':
+		ans = num1 / num2;
+		printf("%lf / %lf = %.3lf", num1, num2, ans);
+		break;
+	
+	default:
+		printf("Invalid request");
+		break;
+	}
+
+
     return 0;
 }
